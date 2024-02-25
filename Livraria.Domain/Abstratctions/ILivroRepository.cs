64e6a1a878db684c54bf9cdfaae9327c932e5094
Willian Yuiti Watanabe <willian.wtnb94@gmail.com>
2024-02-25 14:05:41 -0300

    [FEAT] Criando Interface Livro Repository

---
+++ Livraria.Domain/Abstratctions/ILivroRepository.cs
@@ -0,0 +1,18 @@
+﻿using Livraria.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria.Domain.Abstratctions
+{
+    public interface ILivroRepository
+    {
+        Task<IEnumerable<Livro>> ObterLivros();
+        Task<Livro> ObterLivro(int id);
+        Task<Livro> AdicionarLivro(Livro livro);
+        Task ObterLivros(int id);
+        Task AtualizarLivro(Livro livro);
+    }
+}
